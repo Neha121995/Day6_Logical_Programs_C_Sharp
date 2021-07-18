1615e021c53c8024f0200afd21d5533788f3817c
Neha121995 <jagtapneha1212@gmail.com>
2021-07-18 22:03:30 +0530

    [Neha] added: To Binary Decimal Number Program

---
+++ ToBinary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ToBinaryNumber
+{
+    class ToBinary
+    {
+        public static void toBinary()
+        {
+            Console.Write("Enter number Decimal Number: ");
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            string result = " ";
+
+            while (Num > 1)
+            {
+                int remainder = Num % 2;
+                Num = Num / 2;
+                result = Convert.ToString(remainder) + result;
+
+            }
+            result = Convert.ToString(Num) + result;
+            Console.WriteLine("The Binary number : {0}", result);
+        }
+        static void Main(string[] args)
+        {
+            toBinary();  
+        }
+    }
+}
