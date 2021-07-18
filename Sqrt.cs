727f11d08023891dddf463a112e043a0c4dc9854
Neha121995 <jagtapneha1212@gmail.com>
2021-07-18 22:02:40 +0530

    [Neha] added: To compute the suqare root of nonnegative number Program

---
+++ Sqrt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SqrtProgram
+{
+    class Sqrt
+    {
+        static void Sqrt(int c)
+        {
+            int t = c;
+            long epsilon = (long)10E-15;
+            while (Math.Abs(t - c / t) > epsilon * t)
+            {
+                t = ((c / t) + t) / 2;
+            }
+            Console.WriteLine(t);
+        }
+        static void Main(string[] args)
+        {
+            Sqrt(16);
+        }
+            
+
+    }
+}
