bdaf8c774f0ad1c370738826103662901fcc0b63
Neha121995 <jagtapneha1212@gmail.com>
2021-07-21 01:07:30 +0530

    [Neha] Added: Stopwatch program

---
+++ StopWatch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace StopWatch
+{
+    public class StopWatch
+    {
+        static void Main()
+        {
+            // Create new stopwatch
+            Stopwatch stopwatch = new Stopwatch();
+            // Begin timing
+            String choice ="y";
+            stopwatch.Start();
+            while(choice=="y"||choice == "Y")
+            {
+                    Console.WriteLine("for loop watch press(y)\notherwise stop the watch(n)");
+                    choice = Console.ReadLine();
+                    if (choice == "y" || choice == "Y") {
+                        stopwatch.Stop();
+                        Console.WriteLine("Time Elapsed : {0}", stopwatch.Elapsed);
+                        stopwatch.Start();
+                    }
+                    else if(choice == "n" || choice == "N")
+                    {
+                        break;
+                    }
+                    else {
+                        Console.WriteLine("invalid choice");
+                    }                   
+
+            }
+            // Stop timing
+            stopwatch.Stop();
+            Console.WriteLine("stopwatch stoped : {0}", stopwatch.Elapsed);
+        }
+    }
+}
