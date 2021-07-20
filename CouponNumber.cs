f470bd1e2cbb773fe410fe9edabef4e93066b258
Neha121995 <jagtapneha1212@gmail.com>
2021-07-21 00:32:13 +0530

    [Neha] Added: Generate Coupon Numbers

---
+++ CouponNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CouponNumber
+{
+    class CouponNumber
+    {
+        static void Main(string[] args)
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[6];
+            var random = new Random();
+            Console.WriteLine("How many ticket u want to generate.");
+            int number =int.Parse(Console.ReadLine());
+            while (number>0) {
+                for (int i = 0; i < stringChars.Length; i++)
+                {
+                    stringChars[i] = chars[random.Next(chars.Length)];
+                }
+
+                var finalString = new String(stringChars);
+                Console.WriteLine(finalString);
+                number = number - 1;
+            }
+            
+        }
+    }
+
+}
+
